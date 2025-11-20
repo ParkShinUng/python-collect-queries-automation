613dc5f9d7765bc8ff4d51fb2d2967d703c8757b
ParkShinUng <tlsdnd001@naver.com>
2025-11-20 18:23:47 +0900

    Config 변경

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
 import os
+import platform
+
 from dataclasses import dataclass
 
 
 @dataclass
 class Config:
+    # System
+    platform_info: str = platform.system()
+    
     # Excel
     DATA_DIR = "Data"
     FILE_NAME = "Contents_Seeding.xlsx"
@@ -13,27 +18,28 @@ class Config:
     start_row: int = 2
 
     # ChatGPT
-    __USER_ID = "tlsdnd001@naver.com"
-    __USER_PW = "qlqjqlqj8520"
+    __user_id = "tlsdnd001@gmail.com"
+    __user_pw = "dnddl6290!@"
     chatgpt_url: str = "https://chatgpt.com/"
     check_json_url: str = "https://chatgpt.com/backend-api/conversation/"
     user_data_dir: str = "user_data_chatgpt"
     headless: bool = False
     
     # 병렬 처리
-    num_tabs: int = 10
+    num_tabs: int = 1
 
     # 타임아웃 및 딜레이
-    queries_wait_timeout: float = 5.0
+    queries_wait_timeout: float = 10.0
     reload_wait_timeout: float = 10.0
     max_reload_try: int = 3
     min_answer_wait: float = 3.0
     between_prompts_sleep: float = 1.0
-
+    
     @property
     def USER_ID(self):
-        return self.__USER_ID
+        return self.__user_id
     
     @property
     def USER_PW(self):
-        return self.__USER_PW
+        return self.__user_pw
+    
